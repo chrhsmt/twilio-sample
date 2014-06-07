0b7cdce54137f0fd40f41d64935911e43954729a
chrhsmt <chr@chrhsmt.com>
2014-06-08 02:34:11 +0900

    database.yml for production

--- models/init.rb
+++ models/init.rb
@@ -1,8 +1,8 @@
 require "active_record"
 require_relative 'user'
 
-ActiveRecord::Base.configurations = YAML.load_file('config/database.yml')
 if development?
+    ActiveRecord::Base.configurations = YAML.load_file('config/database.yml')
     ActiveRecord::Base.establish_connection(:development)
 else
     ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
